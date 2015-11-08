64a6761c8c56c7aba173ec3ec662786d227f3791
Susan <susancodes@gmail.com>
2015-11-07 16:53:07 -0800

    added js file

---
+++ static/quizpath.js
@@ -0,0 +1,15 @@
+function showQ2(){
+	$("#question2").show();
+}
+
+function showQ3(){
+	$("#question3").show();
+}
+
+function showQ4(){
+	$("#question4").show();
+}
+
+function showQ5(){
+	$("#question5").show();
+}

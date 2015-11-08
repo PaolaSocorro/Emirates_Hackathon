ddaf7e472e1402ca62642903ff528928965937fa
Paola Socorro <pao.socorro@gmail.com>
2015-11-08 07:09:17 -0800

    not important

--- static/quizpath.js
+++ static/quizpath.js
@@ -16,7 +16,7 @@ function showQ5(){
 
 $( document ).ready(function() {
         console.log( "document loaded" );
-        showTraditionals();
+        $('#load-more-results').hide();
     });
 
 
@@ -63,6 +63,7 @@ function showTraditionals(){
 	$("#question-section").fadeOut(1000);
 	$("#persona").hide();
 	$(".top-three-results").hide();
+	$(".remaining-results").hide();
 	$("#persona").html("<h2>You are a <b>Traditional</b></h2><p>You prefer a life that is more structured, stable and predictable. You would rather follow some set patterns or routines in your life so that you are more likely to know what will happen during each day and, therefore, can plan or prepare for it much better.</p>");
 	$("#persona").fadeIn(5000);
 
@@ -70,7 +71,11 @@ function showTraditionals(){
 }
 
 
-$("#random").on("click", showTraditionals)
+
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
+}
 
 
 
@@ -89,6 +94,57 @@ function getFareResults(persona){
 		var results = data.data;
 
 		for (var i=0; i<3; i++){
+			var r = getRandom(results.length);
+			var city=results[r].city;
+			var lon=results[r].lon;
+			var lat=results[r].lat;
+			var fare=results[r].fares;
+
+			var lowestFare = fare[0].lowestFare;
+			var lowestFareDep = fare[0].departureDateTime;
+			var lowestFareRet = fare[0].returnDateTime;
+			 
+			var fareArray = [];
+			
+			for (var f=0; f < fare.length; f++) {
+			
+				var date = fare[f].departureDateTime.slice(5,10);
+				console.log(date);
+				var dateLowFare = parseInt(fare[f].lowestFare);
+				var dateLowNonStopFare = fare[f].lowestNonStopFare;
+
+				fareArray.push([date, dateLowFare, dateLowNonStopFare]);
+
+				// find the lowest fare
+				// see if there's more than one result
+				// if so, go through each day and compare with the lowestfare
+				// if lower than current lowestfare, update it
+				if (isNaN(fare[f].lowestFare) == false){
+					if (10 < fare[f].lowestFare && fare[f].lowestFare < lowestFare) {
+						lowestFare = fare[f].lowestFare;
+						lowestFareDep = fare[f].departureDateTime;
+						lowestFareRet = fare[f].returnDateTime;
+					}
+				}
+
+			}
+
+			results.splice(r, 1);
+
+			console.log("Lowest: ");
+			console.log(lowestFare);
+			$(".top-three-results").append(
+						"<div class='top-result'><h3>" + city +
+						"</h3><p>Lowest Fare: $" + parseInt(lowestFare) + 
+						"</p><p>Departure Date: " + lowestFareDep.slice(0,10) +
+						"</p><p>Return Date: " + lowestFareRet.slice(0,10) +
+						"</p><button class='experiences-btn' data-lat='" + lat + "' data-lon='" + lon + "' data-toggle='modal' data-target='#experiences-feed'>Things To Do</button></div>")
+			console.log(lowestFareDep);
+			console.log(lowestFareRet);
+
+		}
+
+		for (var i=0; i<results.length; i++){
 			var city=results[i].city;
 			var lon=results[i].lon;
 			var lat=results[i].lat;
@@ -122,23 +178,72 @@ function getFareResults(persona){
 				}
 
 			}
+
 			console.log("Lowest: ");
 			console.log(lowestFare);
-			$(".top-three-results").append(
+			$(".remaining-results").append(
 						"<div class='top-result'><h3>" + city +
 						"</h3><p>Lowest Fare: $" + parseInt(lowestFare) + 
 						"</p><p>Departure Date: " + lowestFareDep.slice(0,10) +
 						"</p><p>Return Date: " + lowestFareRet.slice(0,10) +
-						"</p></div>")
+						"</p><button class='experiences-btn' data-toggle='modal' data-target='#experiences-feed'>Things To Do</button></div>");
 			console.log(lowestFareDep);
 			console.log(lowestFareRet);
 
 		}
+
 	})
 
-	$(".top-three-results").fadeIn(10000);
+
+
+	$(".top-three-results").fadeIn(10000, function() {
+		$("#load-more-results").show();
+	});
 
 }
+
+
+// function showExperiences(){
+// 	var lon = $(this).data("lon");
+// 	// var lat = $(this).data('coords').lat;
+
+// 	console.log("coords" + lon);
+// }
+
+
+// EVENT LISTENERS
+
+$("#random").on("click", showTraditionals);
+
+
+$("#load-more-results").on("click", function() {
+	$("#load-more-results").hide();
+	$(".remaining-results").fadeIn(2000);
+});
+
+
+
+// FUNCTION TO GET XOLA EXPERIENCES
+$(document).on('click', '.experiences-btn', function() {
+
+	var lon = $(this).data("lon");
+	var lat = $(this).data("lat");
+
+	console.log("lon: " + lon);
+	console.log("lat: " + lat);
+
+
+	var url = "/experiencesearch.json?lat=" + lat + 
+				"&lon=" + lon;
+	console.log(url)
+
+
+	// calling for geoJSON
+	$.get(url, function (data) {
+		console.log(data);
+
+	})
+});
 
 
 
